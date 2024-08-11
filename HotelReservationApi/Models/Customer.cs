7eabd2f43a5541252c51b6e19a81dc2ab7ba6b0e
tarekredasaad <redatarek179@gmail.com>
2024-08-12 01:27:45 +0300

    Add project files.

---
+++ HotelReservationApi/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelReservationApi.Models
+{
+    public class Customer:BaseModel
+    {
+        public int UserId {  get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ HotelReservationApi/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelReservationApi.Models
+{
+    public class Room : BaseModel
+    {
+        public string name { get; set; }
+        public Status Status { get; set; }
+        public int RateId { get; set; }
+        public double PriceReservation { get; set; }
+
+    }
+
+    public enum Status
+    {
+        Available,
+        pending,
+        Reserved
+        
+    }
+}
